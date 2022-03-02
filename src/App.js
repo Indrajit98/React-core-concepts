9ea3204a6b37741fc5155104c133168bfa206bf0
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2022-03-03 02:40:20 +0600

    core-concepts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import logo from "./logo.svg";
+import React,{useEffect, useState} from 'react'
 import "./App.css";
 
 function App() {
@@ -12,14 +13,64 @@ function App() {
   return (  
     <div className="App">
       <header className="App-header">
+      <CountNumber></CountNumber>
+      <Users></Users>
+
+
         {
           products.map(pro => <Product product={pro}></Product>)
         }
+
       </header>
     </div>
   );
 
 }
+function Users(){
+  const [users,setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[])
+  return (
+    <div>
+      <h4>User:{users.length}</h4>
+      <ul>
+        {
+          users.map( use => <li>{use.id}{use.name }</li>)
+        }
+        <br></br>
+        {
+          users.map( use => <li>{use.phone}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+function CountNumber(){
+  const [count, setCount ]= useState(0);
+//   const plus = () => {
+//     const newCount= count + 1;
+//     setCount(newCount)
+//   }
+//  const minus = () => {
+//    const newCount = count -1;
+//    setCount(newCount)
+//  }
+// const plus = () => setCount(count +1);
+// const minus =() => setCount(count -1)
+ 
+  return (
+    <div>
+      <h3>Count: {count} </h3>
+      <button onClick={ () => setCount(count +1)}> + </button>
+      <button onClick={() => setCount(count -1)}> - </button>
+    </div>
+  )
+}
+
 function Product(props){
   const productStyle = {
     border:'2px solid red',
