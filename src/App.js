81eabc22264f61a627d2d151dec7f3d6be9b18e1
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2022-03-03 01:05:47 +0600

    product

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
+  const products=[
+  {name:'Photoshop', price:'$56.6'},
+  {name:'Illustrator',price:'$55'},
+  {name:'PDF', price:'$30'},
+  {name:'premier', price: '$40'}
+  ]
+
+  return (  
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          products.map(pro => <Product product={pro}></Product>)
+        }
       </header>
     </div>
   );
+
 }
+function Product(props){
+  const productStyle = {
+    border:'2px solid red',
+    borderRadius:'5px',
+    backgroundColor:'light',
+    height:' 250px',
+    width:' 700px',
+    margin:'10px',
+    float:'left',
+  }
+  
+  return(
+    <div style={productStyle}>
+      <h1>{props.product.name} </h1>
+      <h2>{props.product.price}</h2>
+      <button>button</button>
+    </div>
+  )
+} 
 
 export default App;
